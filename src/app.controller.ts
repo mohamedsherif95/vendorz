04ed2a1eb0154190185f7733fa74338562a798a1
Mohamed Sherif <79464179+mohamedsherif95@users.noreply.github.com>
2023-09-01 22:27:49 +0300

    adding a test json route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  testHandler(@Res() res: Response) {
+    res.json({
+      status: 'SUCCESS'
+    })
   }
 }
