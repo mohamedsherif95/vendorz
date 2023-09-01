45f7c0f3676d3a177626a5192a73c7945532eef9
Mohamed Sherif <79464179+mohamedsherif95@users.noreply.github.com>
2023-09-01 22:53:59 +0300

    update names

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
+  .setTitle('API docs')
+  .setDescription('Documentation for the application APIs')
   .setVersion('1.0')
-  .addTag('cats')
+  .addTag('vendorz')
   .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
